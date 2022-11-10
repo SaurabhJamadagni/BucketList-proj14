fc3c13f3c70a4951363fe9163e9edf48095dbcbd
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-11-10 09:48:53 +0530

    initial commit

---
+++ BucketList/BucketListApp.swift
@@ -0,0 +1,17 @@
+//
+//  BucketListApp.swift
+//  BucketList
+//
+//  Created by Saurabh Jamadagni on 10/11/22.
+//
+
+import SwiftUI
+
+@main
+struct BucketListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
