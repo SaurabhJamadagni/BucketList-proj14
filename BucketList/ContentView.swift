e1c3594408dd3d194d4daa9a4900fc508350de1d
Saurabh Jamadagni <saurabhjamadagni2002@gmail.com>
2022-11-24 15:15:10 +0530

    MapKit introduction

--- BucketList/ContentView.swift
+++ BucketList/ContentView.swift
@@ -5,17 +5,28 @@
 //  Created by Saurabh Jamadagni on 10/11/22.
 //
 
+import MapKit
 import SwiftUI
 
+struct Location: Identifiable {
+    let id = UUID()
+    let name: String
+    let coordinate: CLLocationCoordinate2D
+}
+
 struct ContentView: View {
+    @State private var mapRegion = MKCoordinateRegion(center: CLLocationCoordinate2D(latitude: 51.5, longitude: -0.12),
+                                                      span: MKCoordinateSpan(latitudeDelta: 0.2, longitudeDelta: 0.2))
+    
+    let locations = [
+        Location(name: "Buckingham Palace", coordinate: CLLocationCoordinate2D(latitude: 51.501, longitude: -0.141)),
+        Location(name: "Tower of London", coordinate: CLLocationCoordinate2D(latitude: 51.508, longitude: -0.076))
+    ]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        Map(coordinateRegion: $mapRegion, annotationItems: locations) { location in
+            MapMarker(coordinate: location.coordinate)
         }
-        .padding()
     }
 }
 
